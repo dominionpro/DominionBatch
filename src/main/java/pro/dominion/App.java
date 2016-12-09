4e8514c40cc4b2326f9bcb30fb64d91dcc16703b
dominionpro <info@dominion.pro>
2016-12-09 17:10:16 +0100

    initial commit

---
+++ src/main/java/pro/dominion/App.java
@@ -0,0 +1,13 @@
+package pro.dominion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
